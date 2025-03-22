5ab50f9073359fe95a07a45ac6897f466098ac94
hixvm <hixvmx@gmail.com>
2025-03-22 01:24:31 +0000

    feat: Design Hero section

---
+++ src/components/Pages/Home/Hero.jsx
@@ -0,0 +1,23 @@
+import { VoiceIcon } from "@/app/db/icons";
+
+
+export default function Hero() {
+
+    return (
+        <section>
+            <div className="w-full max-w-6xl mx-auto flex flex-col items-center justify-center text-center min-h-[60dvh] gap-3">
+                <a href="/">
+                    <strong className="textBody-l1 text-black select-none">SoundPost</strong>
+                </a>
+                <h1 className="text-6xl leading-[70px] font-[800]">Voice posts made simple.</h1>
+                <p className="textBody-ml3">Easily create and share voice posts on social media.</p>
+                <a href="#record">
+                    <button className="mt-6 textBody-m2 bg-primary-1 py-3 px-6 rounded-md hover:bg-primary-2 transition-colors duration-200 text-white flex items-center gap-2">
+                        <VoiceIcon className="size-8" />
+                        <span className="whitespace-nowrap">Start recording</span>
+                    </button>
+                </a>
+            </div>
+        </section>
+    )
+}
--- src/components/Pages/Home/index.jsx
+++ src/components/Pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header";
+import Hero from "./Hero";
 
 
 export default function HomePage() {
     return (
         <div>
             <Header />
+            <Hero />
         </div>
     )
 }
